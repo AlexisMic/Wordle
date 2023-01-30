1072b06fe7bd8a21f94ec982f17410302e0a673b
Alexis Schotte <amschotte@me.com>
2023-01-30 12:44:50 +0000

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Alexis Schotte on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
